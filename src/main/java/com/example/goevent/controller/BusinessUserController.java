3e85204828eb6adfbb190edb3244265cbabccf87
phucgari <phucgari@gmail.com>
2023-03-18 12:51:07 +0700

    add controllers

---
+++ src/main/java/com/example/goevent/controller/BusinessUserController.java
@@ -0,0 +1,33 @@
+package com.example.goevent.controller;
+
+import com.example.goevent.model.BusinessUser;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class BusinessUserController implements GenericController<BusinessUser>{
+    @Override
+    public ArrayList<BusinessUser> showAll() {
+        return null;
+    }
+
+    @Override
+    public void create(BusinessUser object) {
+
+    }
+
+    @Override
+    public BusinessUser showByIndex(int index) {
+        return null;
+    }
+
+    @Override
+    public void update(BusinessUser object) {
+
+    }
+
+    @Override
+    public void delete(int index) {
+
+    }
+}
